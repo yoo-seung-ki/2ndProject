663528a98d6975da66854269419a276771546121
BlancPong <bu3820@naver.com>
2022-06-24 15:51:21 +0900

    기업 사용자들의 VO

---
+++ main/java/common/CompanyVO.java
@@ -0,0 +1,275 @@
+package common;
+
+public class CompanyVO {
+	private int Companyseq;
+	private String Companyname;
+	private String Ceo;
+	private String Createyear;
+	private String Address;
+	private String Homepage;
+	private String Companytype;
+	private String Companycontent;
+	private String Companysize;
+	private String Recrutype;
+	private String Workcontent;
+	private String Graduate;
+	private String Employcase;
+	private String Paytype;
+	private String Severance;
+	private String Worktime;
+	private String Etc;
+	private int	Recrusize;
+	private String Workarea;
+	private String Career;
+	private String Worktype;
+	private String Insurance;
+	private String Bonus;
+	private String Addwork;
+	private String Major;
+	private String Computer;
+	private String Welfare;
+	private String License;
+	private String Facil;
+	private String Treatment;
+	
+
+	public int getCompanyseq() {
+		return Companyseq;
+	}
+
+	public void setCompanyseq(int companyseq) {
+		Companyseq = companyseq;
+	}
+
+	public String getCompanyname() {
+		return Companyname;
+	}
+
+	public void setCompanyname(String companyname) {
+		Companyname = companyname;
+	}
+
+	public String getCeo() {
+		return Ceo;
+	}
+
+	public void setCeo(String ceo) {
+		Ceo = ceo;
+	}
+
+	public String getCreateyear() {
+		return Createyear;
+	}
+
+	public void setCreateyear(String createyear) {
+		Createyear = createyear;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getHomepage() {
+		return Homepage;
+	}
+
+	public void setHomepage(String homepage) {
+		Homepage = homepage;
+	}
+
+	public String getCompanytype() {
+		return Companytype;
+	}
+
+	public void setCompanytype(String companytype) {
+		Companytype = companytype;
+	}
+
+	public String getCompanycontent() {
+		return Companycontent;
+	}
+
+	public void setCompanycontent(String companycontent) {
+		Companycontent = companycontent;
+	}
+
+	public String getCompanysize() {
+		return Companysize;
+	}
+
+	public void setCompanysize(String companysize) {
+		Companysize = companysize;
+	}
+
+	public String getRecrutype() {
+		return Recrutype;
+	}
+
+	public void setRecrutype(String recrutype) {
+		Recrutype = recrutype;
+	}
+
+	public String getWorkcontent() {
+		return Workcontent;
+	}
+
+	public void setWorkcontent(String workcontent) {
+		Workcontent = workcontent;
+	}
+
+	public String getGraduate() {
+		return Graduate;
+	}
+
+	public void setGraduate(String graduate) {
+		Graduate = graduate;
+	}
+
+	public String getEmploycase() {
+		return Employcase;
+	}
+
+	public void setEmploycase(String employcase) {
+		Employcase = employcase;
+	}
+
+	public String getPaytype() {
+		return Paytype;
+	}
+
+	public void setPaytype(String paytype) {
+		Paytype = paytype;
+	}
+
+	public String getSeverance() {
+		return Severance;
+	}
+
+	public void setSeverance(String severance) {
+		Severance = severance;
+	}
+
+	public String getWorktime() {
+		return Worktime;
+	}
+
+	public void setWorktime(String worktime) {
+		Worktime = worktime;
+	}
+
+	public String getEtc() {
+		return Etc;
+	}
+
+	public void setEtc(String etc) {
+		Etc = etc;
+	}
+
+	public int getRecrusize() {
+		return Recrusize;
+	}
+
+	public void setRecrusize(int recrusize) {
+		Recrusize = recrusize;
+	}
+
+	public String getWorkarea() {
+		return Workarea;
+	}
+
+	public void setWorkarea(String workarea) {
+		Workarea = workarea;
+	}
+
+	public String getCareer() {
+		return Career;
+	}
+
+	public void setCareer(String career) {
+		Career = career;
+	}
+
+	public String getWorktype() {
+		return Worktype;
+	}
+
+	public void setWorktype(String worktype) {
+		Worktype = worktype;
+	}
+
+	public String getInsurance() {
+		return Insurance;
+	}
+
+	public void setInsurance(String insurance) {
+		Insurance = insurance;
+	}
+
+	public String getBonus() {
+		return Bonus;
+	}
+
+	public void setBonus(String bonus) {
+		Bonus = bonus;
+	}
+
+	public String getAddwork() {
+		return Addwork;
+	}
+
+	public void setAddwork(String addwork) {
+		Addwork = addwork;
+	}
+
+	public String getMajor() {
+		return Major;
+	}
+
+	public void setMajor(String major) {
+		Major = major;
+	}
+
+	public String getComputer() {
+		return Computer;
+	}
+
+	public void setComputer(String computer) {
+		Computer = computer;
+	}
+
+	public String getWelfare() {
+		return Welfare;
+	}
+
+	public void setWelfare(String welfare) {
+		Welfare = welfare;
+	}
+
+	public String getLicense() {
+		return License;
+	}
+
+	public void setLicense(String license) {
+		License = license;
+	}
+
+	public String getFacil() {
+		return Facil;
+	}
+
+	public void setFacil(String facil) {
+		Facil = facil;
+	}
+
+	public String getTreatment() {
+		return Treatment;
+	}
+
+	public void setTreatment(String treatment) {
+		Treatment = treatment;
+	}
+}

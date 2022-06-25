b3fda96d77661b2774eb0a312dc2c1180dd9aa42
DESKTOP-BND0HI7\sgsg9 <ysgsl@naver.com>
2022-06-25 17:21:06 +0900

    CompanyDAO 에 모든 기업의 모든 정보를 List 타입으로 반환하는 메소드 추가

--- main/java/common/CompanyDAO.java
+++ main/java/common/CompanyDAO.java
@@ -67,4 +67,60 @@ public class CompanyDAO {
 		return vo;
 	}
 	
+	// 모든 기업의 모든 정보를 담은 list반환
+	// ArrayList<String> 형태로 반환
+	public List<CompanyVO> getCompanyList() {
+		List<CompanyVO> list = new ArrayList<CompanyVO>();
+		Connection con = null;
+		PreparedStatement pstmt = null;	
+		String sql = null;
+		ResultSet rs = null;
+		try {
+			con = pool.getConnection();
+			sql = "select * from mjt order by companyseq";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				CompanyVO vo = new CompanyVO();
+				vo.setCompanyseq(rs.getInt("Companyseq"));
+				vo.setCompanyname(rs.getString("Companyname"));
+				vo.setCeo(rs.getString("Ceo"));
+				vo.setCreateyear(rs.getString("Createyear"));
+				vo.setAddress(rs.getString("Address"));
+				vo.setHomepage(rs.getString("Homepage"));
+				vo.setCompanytype(rs.getString("Companytype"));
+				vo.setCompanycontent(rs.getString("Companycontent"));
+				vo.setCompanysize(rs.getString("Companysize"));
+				vo.setRecrutype(rs.getString("Recrutype"));
+				vo.setWorkcontent(rs.getString("Workcontent"));
+				vo.setGraduate(rs.getString("Graduate"));
+				vo.setEmploycase(rs.getString("Employcase"));
+				vo.setPaytype(rs.getString("Paytype"));
+				vo.setSeverance(rs.getString("Severance"));
+				vo.setWorktime(rs.getString("Worktime"));
+				vo.setEtc(rs.getString("Etc"));
+				vo.setRecrusize(rs.getInt("Recrusize"));
+				vo.setWorkarea(rs.getString("Workarea"));
+				vo.setCareer(rs.getString("Career"));
+				vo.setWorktype(rs.getString("Worktype"));
+				vo.setInsurance(rs.getString("Insurance"));
+				vo.setBonus(rs.getString("Bonus"));
+				vo.setAddwork(rs.getString("Addwork"));
+				vo.setMajor(rs.getString("Major"));
+				vo.setComputer(rs.getString("Computer"));
+				vo.setWelfare(rs.getString("Welfare"));
+				vo.setLicense(rs.getString("License"));
+				vo.setFacil(rs.getString("Facil"));
+				vo.setTreatment(rs.getString("Treatment"));
+				list.add(vo);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return list;
+	}
+
+
 }
